cf00c76ed47d72fcb431b221486a6d8ca3fd274b
Shamrock <gamewithbob1@gmail.com>
2025-04-17 23:53:31 +0000

    edit

---
+++ Scripts/base.js
@@ -0,0 +1,10 @@
+function loadIframe(url, target) {
+    var iframe=document.getElementById(target);
+    switch (url)
+
+    {       
+         case 'kedrio':
+            iframe.src = 'https://kedr.io/webapp/?updated=1.1.16#/room/!qxKOSapuIdVpwGhwWF:kedr.io'; // URL for Kedrio
+            break;
+        }
+    }
